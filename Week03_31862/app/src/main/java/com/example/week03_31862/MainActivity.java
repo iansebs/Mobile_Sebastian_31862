b7e26a02af2b852d770cc889508b6614e57a981c
ian <64369602+iansebs@users.noreply.github.com>
2021-02-12 17:36:32 +0700

    week 3

---
+++ Week03_31862/app/src/main/java/com/example/week03_31862/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.week03_31862;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private DaftarKataAdapter mAdapter;
+
+    private final LinkedList<String> mDaftarKata = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int jumlahkata = mDaftarKata.size();
+                mDaftarKata.addLast("kata" +(jumlahkata + 1)+ "telah ditambahkan");
+
+                Objects.requireNonNull(
+                        mRecyclerView.getAdapter()).notifyItemInserted(jumlahkata);
+                mRecyclerView.smoothScrollToPosition(jumlahkata);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        for (int i = 1; i < 21; i++){
+            mDaftarKata.add("Kata " + i);
+        }
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mAdapter = new DaftarKataAdapter(this, mDaftarKata);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
